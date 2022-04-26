a6df89b04e32a5bdc2441bc0cc83c718976251fe
David Rodríguez <deivid.rodriguez@riseup.net>
2022-04-26 09:35:27 +0200

    Better solution to support truffleruby platform handling

--- bundler/lib/bundler/dependency.rb
+++ bundler/lib/bundler/dependency.rb
@@ -109,7 +109,7 @@ module Bundler
       @env            = options["env"]
       @should_include = options.fetch("should_include", true)
       @gemfile        = options["gemfile"]
-      @force_ruby_platform = options["force_ruby_platform"]
+      @force_ruby_platform = options.fetch("force_ruby_platform", default_force_ruby_platform)
 
       @autorequire = Array(options["require"] || []) if options.key?("require")
     end
@@ -158,6 +158,14 @@ module Bundler
       super
     rescue NoMethodError
       requirement != ">= 0"
+    end
+
+    private
+
+    def default_force_ruby_platform
+      return false unless Bundler.current_ruby.truffleruby?
+
+      !Gem::Platform::REUSE_AS_BINARY_ON_TRUFFLERUBY.include?(name)
     end
   end
 end
